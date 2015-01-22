4d23a60435b87e9bfb91f084cc4d0c781d087d8f
Daniel Nolan <daniel@dnolan.com>
2015-01-22 22:48:34 +0000

    Fixed most of the links

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,6 @@ var mainApp = angular.module('mainApp', [
   'mainControllers'
 ]);
 
-
 mainApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.when('/', {
@@ -15,5 +14,17 @@ mainApp.config(['$routeProvider',
       templateUrl: 'partials/cv.html'
     }).when('/portfolio', {
       templateUrl: 'partials/portfolio.html'
+    }).when('/portfolio_bw', {
+      templateUrl: 'partials/portfolio_bw.html'
+    }).when('/portfolio_game', {
+      templateUrl: 'partials/portfolio_game.html'
+    }).when('/portfolio_nadger', {
+      templateUrl: 'partials/portfolio_nadger.html'
+    }).when('/portfolio_other', {
+      templateUrl: 'partials/portfolio_other.html'
+    }).when('/portfolio_q2', {
+      templateUrl: 'partials/portfolio_q2.html'
+    }).when('/portfolio_unused', {
+      templateUrl: 'partials/portfolio_unused.html'
     });
   }]);
